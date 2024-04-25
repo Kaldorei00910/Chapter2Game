cb8dc05acec51dc76ec273041feb51829a1792b2
Kaldorei00910 <117884183+Kaldorei00910@users.noreply.github.com>
2024-04-25 17:07:13 +0900

    텍스트 및 수치 조정. "진짜제출용"

--- Chapter2Game/Program.cs
+++ Chapter2Game/Program.cs
@@ -24,10 +24,10 @@ namespace Chapter2Game
             Item mu_shuePlate = new Item("무쇠갑옷      ", "방어력", 5, "", "무쇠로 만들어져 튼튼한 갑옷입니다           ", 1000);
             Item spartaSpear = new Item("스파르타의 창", "공격력", 12, "", "스파르타의 전사들이 사용했다는 전설의 창입니다.", 1200);
             Item oldSword = new Item("낡은 검       ", "공격력", 2, "", "쉽게 볼 수 있는 낡은 검 입니다.                  ", 600);
-            Item workPlate = new Item("수련자 갑옷  ", "방어력", 5, "", "수련에 도움을 주는 갑옷입니다.                   ", 1000);
+            Item workPlate = new Item("수련자 갑옷  ", "방어력", 8, "", "수련에 도움을 주는 갑옷입니다.                   ", 1000);
             Item bronzeAxe = new Item("청동 도끼    ", "공격력", 5, "", "어디선가 사용됐던거 같은 도끼입니다.              ", 1500);
             Item spartaPlate = new Item("스파르타의 갑옷", "방어력", 15, "", "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 3500);
-            Item stone = new Item("돌맹이       ", "공격력", 1, "", "그냥 돌입니다.                           ", 1);
+            Item stone = new Item("애완 돌       ", "공격력", 1, "", "땅에서 주운 당신의 동반자입니다                         ", 1);
 
             Dungeon level1 = new Dungeon(1, 1000, 5);
             Dungeon level2 = new Dungeon(2, 1700, 11);
@@ -38,16 +38,15 @@ namespace Chapter2Game
             DungeonList.Add(level3);
 
             allitemList.Add(stone);
-            allitemList.Add(mu_shuePlate);
-            allitemList.Add(spartaSpear);
             allitemList.Add(oldSword);
-            allitemList.Add(workPlate);
             allitemList.Add(bronzeAxe);
+            allitemList.Add(spartaSpear);
+            allitemList.Add(mu_shuePlate);
+            allitemList.Add(workPlate);
             allitemList.Add(spartaPlate);
 
-            invenList.Add(mu_shuePlate);
-            invenList.Add(oldSword);
-            invenList.Add(spartaSpear);
+            invenList.Add(stone);
+
 
             while (true)
             {
@@ -246,7 +245,7 @@ namespace Chapter2Game
                         {
                             if (random.Next(1, 10) <= 4)//확률적 실패한 경우
                             {
-                                if (player.hp == 1)//체력이 10이하로 던전 들어가 실패 경우 : 사망
+                                if (player.hp <= 10)//체력이 10이하로 던전 들어가 실패 경우 : 사망
                                 {
                                     Console.Clear();
                                     Console.ForegroundColor = ConsoleColor.DarkRed;
